7afb8a7aa6c7e2d03511c01ce0580d9ed8236960
Markus <Markus@MARKUS-BKR>
2024-05-23 09:12:26 +0200

    Projektdateien hinzufügen.

---
+++ AutoPersonGewicht/Auto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoPersonGewicht
+{
+    public class Auto
+    {
+        private double gesamtgewicht;
+        private double fahrzeuggewicht;
+        private int anzahlPassagiere;
+        List<Person> person = new List<Person>();
+
+        public Auto()
+        {
+
+        }
+        public Auto(double pFahrzeuggewicht)
+        {
+            fahrzeuggewicht = pFahrzeuggewicht;
+            gesamtgewicht = fahrzeuggewicht;
+        }
+
+        public double GetGesamtgewicht(double pFahrzeuggewicht, Person pPerson)
+        {
+            gesamtgewicht = pFahrzeuggewicht + pPerson.GetGewicht();
+            return gesamtgewicht;
+        }
+
+        public void Einsteigen(Person pPerson)
+        {
+            person.Add(pPerson);
+        }
+
+        public void Aussteigen(Person pPerson)
+        {
+            person.Remove(pPerson);
+        }
+
+
+        public int GetAnzahl()
+        {
+            return anzahlPassagiere;
+        }
+    }
+}
---
+++ AutoPersonGewicht/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoPersonGewicht
+{
+    public class Person
+    {
+        private double gewicht;
+
+        public Person(double pGewicht)
+        {
+            gewicht = pGewicht;
+        }
+
+        public double GetGewicht()
+        {
+            return gewicht;
+        }
+    }
+}
